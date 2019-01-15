d251fcb01df41962b594c42485b63423fd39bcfc
pavelaron <junkieman@freemail.hu>
2019-01-15 13:01:42 +0100

    Fixed blockly not loaded on Android bug & added more blocks

--- app/components/BlocklyComponent/index.js
+++ app/components/BlocklyComponent/index.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import {
-  View
+  Platform, View
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default class BlocklyComponent extends Component {
+
+  renderWebView = isAndroid => {
+    if (isAndroid) {
+      return (
+        <WebView
+          source={{ uri: 'file:///android_asset/blockly/index.html' }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+        />
+      );
+    }
+
+    return(
+      <WebView
+        source={require('./blockly.html')}
+      />
+    );
+  };
+
   render() {
     return (
       <View {...this.props}>
-        <WebView
-          source={require('./blockly.html')}
-        />
+        { this.renderWebView(Platform.OS === 'android') }
       </View>
     );
   }
