992e6d0e16554e00caa2df68531ff3cd04eb6a53
pavelaron <junkieman@freemail.hu>
2019-01-15 16:55:08 +0100

    Implemented bridge between webview and blockly component

--- app/components/BlocklyComponent/index.js
+++ app/components/BlocklyComponent/index.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
-import {
-  Platform, View
-} from 'react-native';
+import { Platform, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default class BlocklyComponent extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      xmlData: ''
+    };
+  }
 
   renderWebView = isAndroid => {
     if (isAndroid) {
       return (
         <WebView
           source={{ uri: 'file:///android_asset/blockly/index.html' }}
+          onMessage={this.onWebViewMessage}
           javaScriptEnabled={true}
           domStorageEnabled={true}
         />
       );
     }
 
-    return(
+    return (
       <WebView
         source={require('./blockly.html')}
+        onMessage={this.onWebViewMessage}
+        useWebKit={true}
       />
     );
+  };
+
+  onWebViewMessage = event => {
+    this.setState({
+      xmlData: event.nativeEvent.data
+    });
   };
 
   render() {
