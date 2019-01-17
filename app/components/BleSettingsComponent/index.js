bb03130d89237baf6efdeeace925097c45c28859
pavelaron <junkieman@freemail.hu>
2019-01-17 17:44:09 +0100

    Implemented device listing & connect capabilities for BLE module

--- app/components/BleSettingsComponent/index.js
+++ app/components/BleSettingsComponent/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { 
+  StyleSheet,
   Platform, 
   PermissionsAndroid,
   Alert, 
   View, 
-  Text 
+  Text,
+  FlatList,
+  TouchableOpacity
 } from 'react-native';
 import { BleManager } from 'react-native-ble-plx';
 
@@ -15,7 +18,8 @@ export default class BleSettingsComponent extends Component {
     this.manager = new BleManager();
 
     this.state = {
-      foundDevices: []
+      foundDevices: [],
+      emptyString: 'Initializing BLE device list...'
     }
   }
 
@@ -29,17 +33,42 @@ export default class BleSettingsComponent extends Component {
 
   render() {
     return (
-      <View>
-        <Text>BLE settings placeholder...</Text>
+      <View style={styles.container}>
+        {this.renderListOrEmpty()}
       </View>
     );
   }
 
+  renderListOrEmpty = () => {
+    return this.state.foundDevices.length === 0 ?
+    (<Text style={{styles.emptyString}}>{this.state.emptyString}</Text>) : (
+      <FlatList 
+        style={styles.deviceList}
+        contentContainerStyle={styles.listBottomPadding}
+        data={this.state.foundDevices}
+        renderItem={this.renderItem}
+        keyExtractor={(item) => item.id}
+        removeClippedSubviews={true}
+      />
+    );
+  };
+
+  renderItem = ({item, index}) => (
+    <TouchableOpacity style={styles.deviceItem} onPress={() => this.connect(item)}>
+      <Text style={styles.deviceItemTitle}>{item.localName || item.id}</Text>
+    </TouchableOpacity>
+  );
+
   setStateChangeListener = () => {
     const subscription = this.manager.onStateChange(state => {
       if (state === 'PoweredOn') {
-        this.scanAndConnect();
+        this.scanDevices();
         subscription.remove();
+      } else {
+        this.setState({
+          foundDevices: [],
+          emptyString: 'Please enable Bluetooth to start scanning for devices...'
+        });
       }
     }, true);
   };
@@ -73,42 +102,89 @@ export default class BleSettingsComponent extends Component {
     );
   };
 
-  scanAndConnect = () => {
+  scanDevices = () => {
+    this.setState({
+      emptyString: 'Searching for BLE devices...'
+    });
+
     this.manager.startDeviceScan(null, null, (error, device) => {
       if (error) {
         // Handle error (scanning will be stopped automatically)
         this.showError(error);
         return;
-      } else if (this.state.foundDevices.includes(device.id)) {
-        return;
-      }
+      //} else if (this.state.foundDevices.includes(device) || !device.name) {
+      } else {
+        const deviceInList = this.state.foundDevices.some(element => {
+          return element.id === device.id;
+        });
 
-      this.setState({
-        ...this.state,
-        foundDevices: [
-          ...this.state.foundDevices,
-          device.id
-        ]
-      })
+        if (deviceInList) {
+          return;
+        }
+      }
 
       console.log(device);
 
-      // Check if it is a device you are looking for based on advertisement data
-      // or other criteria.
-      if (device.name === 'Revvy_RPi') {
-        
-        // Stop scanning as it's not necessary if you are scanning for one device.
-        this.manager.stopDeviceScan();
-
-        // Proceed with connection.
-        device.connect()
-          .then(device => device.discoverAllServicesAndCharacteristics())
-          .then(device => {
-            console.error(device.name);
-            // Do work on device with services and characteristics
-          })
-          .catch(error => this.showError(error));
-      }
+      this.setState({
+        foundDevices: [
+          ...this.state.foundDevices,
+          device
+        ]
+      });
     });
   };
+
+  connect = device => {
+    // Stop scanning as it's not necessary if you are scanning for one device.
+    this.manager.stopDeviceScan();
+
+    // Proceed with connection.
+    device.connect()
+      .then(device => device.discoverAllServicesAndCharacteristics())
+      .then(device => {
+        console.log('LISTING DEVICE CHARACTERISTICS...');
+        console.log(device);
+        // Do work on device with services and characteristics
+      })
+      .catch(error => this.showError(error));
+  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignSelf: 'stretch',
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  deviceList: {
+    flex: 1,
+    alignSelf: 'stretch',
+    paddingBottom: 20
+  },
+  listBottomPadding: {
+    paddingBottom: 50
+  },
+  emptyString: {
+    padding: 30
+  },
+  deviceItem: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    marginTop: 20,
+    marginHorizontal: 20,
+    borderRadius: 2,
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    shadowOffset: {
+      height: 1,
+      width: 0.3,
+    },
+    elevation: 3
+  },
+  deviceItemTitle: {
+    padding: 20
+  }
+});
