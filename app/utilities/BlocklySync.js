a465a0d69d08e937b405cafd82cf78ebef25ec75
pavelaron <junkieman@freemail.hu>
2019-02-25 13:23:23 +0100

    Added capability of sending synced Blockly scripts in packets

--- app/utilities/BlocklySync.js
+++ app/utilities/BlocklySync.js
@@ -2,22 +2,42 @@ import base64 from 'base64-js';
 
 export default class BlocklySync {
 
+  static packetSize = 512;
+
   static sync = (props) => {
     const uartCharacteristic = props.uartCharacteristic;
-    const savedList = props.savedList.map(item => (item.pythonCode || ''));
+    if (!uartCharacteristic) {
+      return;
+    }
 
+    const savedList = props.savedList.map(item => (item.pythonCode || ''));
     const encoded = encodeURIComponent(JSON.stringify(savedList));
     const listByteArray = encoded.split('').map(c => c.charCodeAt());
 
-    const byteArray = new Uint8Array([
+    try {
+      BlocklySync.sendPacket(uartCharacteristic, listByteArray, 0);
+    } catch (e) {
+      console.warn(e.message);
+    }
+  };
+
+  static sendPacket = (characteristic, array, index) => {
+    const firstByte = index * BlocklySync.packetSize;
+    const lastByte = firstByte + BlocklySync.packetSize;
+
+    const packetArray = new Uint8Array([
       0xfe,
-      ...listByteArray
+      (lastByte >= array.length) | 0,
+      ...(array.slice(firstByte, lastByte))
     ]);
 
     try {
-      const base64Data = base64.fromByteArray(byteArray);
-      uartCharacteristic.writeWithResponse(base64Data)
-        .then()
+      characteristic.writeWithResponse(base64.fromByteArray(packetArray))
+        .then(() => {
+          if (lastByte <= array.length) {
+            BlocklySync.sendPacket(characteristic, array, index + 1);
+          }
+        })
         .catch(e => console.warn(e.message));
     } catch (e) {
       console.warn(e.message);
