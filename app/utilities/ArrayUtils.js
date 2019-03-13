3fd7963aa5767967a816cbef4184f51b15267a5e
pavelaron <junkieman@freemail.hu>
2019-03-13 13:40:59 +0100

    New ArrayUtils.chunk function

--- app/utilities/ArrayUtils.js
+++ app/utilities/ArrayUtils.js
@@ -1,15 +1,13 @@
 export default class ArrayUtils {
 
   static chunk(array, size) {
-    const chunkedArray = [];
+    return array.reduce((chunkedArray, item, index) => {
+      if (index % size === 0) {
+        chunkedArray.push([]);
+      }
 
-    let copied = [...array]; // ES6 destructuring
-    const numOfChild = Math.ceil(copied.length / size);
-
-    for (let i = 0; i < numOfChild; i++) {
-      chunkedArray.push(copied.splice(0, size));
-    }
-    
-    return chunkedArray;
-  };
+      chunkedArray[chunkedArray.length - 1].push(item);
+      return chunkedArray;
+    }, []);
+  }
 }
