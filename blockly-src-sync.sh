4de4fdfd681021176bfa57f215a16558bae02fa2
Pável Áron <aron@revolutionrobotics.org>
2019-05-17 10:33:36 +0200

    Removed unused Blockly src sync script

--- blockly-src-sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rsync -av --delete \
-  --exclude='package.json' \
-  --exclude='appengine' \
-  --exclude='demos' \
-  --exclude='tests' \
-    ./node_modules/blockly/ ./assets/blockly/scripts/blockly/
